2b9958e4f7b027cdd099bced8746df35bdd1b88a
Alex Medeiros <alex.follador.medeiros@gmail.com>
2025-07-09 18:46:20 +1000

    j

--- src/1027/Model/CEmployeeV3.py
+++ src/1027/Model/CEmployeeV3.py
@@ -3,7 +3,7 @@ import argparse
 import pygame
 import sys
 sys.path.append("..\\..\\lib")
-from mylogger_v3_2 import mylogger,mylog_section,myic,DISPLAY_SELF,DISPLAY_PARAM,DISPLAY_STATE,DISPLAY_STD,DISPLAY_FULL
+from mylogger_v3_3 import mylogger,mylog_section,myic,DISPLAY_SELF,DISPLAY_PARAM,DISPLAY_STATE,DISPLAY_STD,DISPLAY_FULL
 import time
 
 pygame.init()
@@ -83,10 +83,9 @@ class CEmployee:
         r1 = 0
         r2 = 0
         r3 = 0
-        if self.ps != None:
-            nephew = self.ps.get_last_child()
-            if nephew != None:
-                r3 = 1
+        nephew = self.ps.get_last_child()  if  (self.ps != None) else None
+        if nephew != None:
+            r3 = 1
         if self.ps != None:
             r2 = 1 
         if self.ps == None:
@@ -144,11 +143,9 @@ class CEmployee:
         r3 = 0
         if self.parent == None:
             r0 = 1 
-        nephew = None
-        if self.ps != None:
-            nephew = self.ps.get_last_child()
-            if nephew != None:
-                r3 = 1
+        nephew = self.ps.get_last_child()  if  (self.ps != None) else None
+        if nephew != None:
+            r3 = 1
         if self.ps == None: #no ps
             r1 = 1
         if self.ps != None: #ps
@@ -219,6 +216,19 @@ if __name__ == "__main__":
         root_obj = CEmployee(guid_p = 'root',name_p='root',w_p=100, parent_p = None)
         a = CEmployee(guid_p = 'a',name_p='a',w_p=100, parent_p = root_obj)
         b = CEmployee(guid_p = 'b',name_p='b',w_p=300, parent_p = root_obj)
+        root_obj.add_child(child_p = a)
+        root_obj.add_child(child_p = b)
+        root_obj.y = 0
+        root_obj.rh = 100
+        a.y = 100
+        a.rh = 100
+        b.y = 100
+        b.rh = 100
+        return root_obj
+    def create_tree_for_testing_3():
+        root_obj = CEmployee(guid_p = 'root',name_p='root',w_p=100, parent_p = None)
+        a = CEmployee(guid_p = 'a',name_p='a',w_p=100, parent_p = root_obj)
+        b = CEmployee(guid_p = 'b',name_p='b',w_p=300, parent_p = root_obj)
         c = CEmployee(guid_p = 'c',name_p='c',w_p=100, parent_p = a)
         d = CEmployee(guid_p = 'd',name_p='d',w_p=100, parent_p = a)
         root_obj.add_child(child_p = a)
@@ -242,55 +252,26 @@ if __name__ == "__main__":
     parser.add_argument('-s','--scenario', help='testing', required=True)
     args = vars(parser.parse_args())
     if args['test'] == 'rxd':
-        mylog_section('intialising tree')
+        mylog_section('intialising tree')       
         if args['scenario'] == '0':
             root_obj = create_tree_for_testing_0()
-            root_obj.calc_i_self(0)
-            root_obj.calc_ps_tree()
-            root_obj.calc_aw_tree()
-            longest_length = root_obj.get_longest_distance_tree(0)
-            scale_xd = SCREEN_WIDTH / longest_length
-            root_obj.calc_rw_tree(scale_xd_p = scale_xd)
-            root_obj.calc_raw_tree(scale_xd_p = scale_xd)
-            root_obj.calc_x_tree()
-            root_obj.draw_tree()
         if args['scenario'] == '1':
             root_obj = create_tree_for_testing_1()
-            root_obj.calc_i_self(0)
-            root_obj.calc_ps_tree()
-            root_obj.calc_aw_tree()
-            longest_length = root_obj.get_longest_distance_tree(0)
-            scale_xd = SCREEN_WIDTH / longest_length
-            root_obj.calc_rw_tree(scale_xd_p = scale_xd)
-            root_obj.calc_raw_tree(scale_xd_p = scale_xd)
-            root_obj.calc_x_tree()
-            root_obj.draw_tree()
         if args['scenario'] == '2':
             root_obj = create_tree_for_testing_2()
-            root_obj.calc_i_self(0)
-            root_obj.calc_ps_tree()
-            root_obj.calc_aw_tree()
-            longest_length = root_obj.get_longest_distance_tree(0)
-            scale_xd = SCREEN_WIDTH / longest_length
-            root_obj.calc_rw_tree(scale_xd_p = scale_xd)
-            root_obj.calc_raw_tree(scale_xd_p = scale_xd)
-            root_obj.calc_r_space(scale_xd_p = scale_xd)
-            root_obj.calc_x_tree()
-            root_obj.draw_tree()
-        mylog_section('printing tree')
+        if args['scenario'] == '3':
+            root_obj = create_tree_for_testing_3()    
         root_obj.print_tree()
-        mylog_section('calculating ps')
         root_obj.calc_i_self(0)
         root_obj.calc_ps_tree()
         root_obj.calc_aw_tree()
         longest_length = root_obj.get_longest_distance_tree(0)
-        myic(longest_length)
         scale_xd = SCREEN_WIDTH / longest_length
-        myic(scale_xd)
-
         root_obj.calc_rw_tree(scale_xd_p = scale_xd)
         root_obj.calc_raw_tree(scale_xd_p = scale_xd)
+        root_obj.calc_r_space(scale_xd_p = scale_xd)
         root_obj.calc_x_tree()
+        root_obj.draw_tree()
         
         pygame.display.update()  
         run = True        
