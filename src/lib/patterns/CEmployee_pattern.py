9815a2857af90f0935f74dd2d6db70362ff428f5
Alex Medeiros <alex.follador.medeiros@gmail.com>
2025-08-11 21:18:12 +1000

    toggle activation status

--- src/lib/patterns/CEmployee_pattern.py
+++ src/lib/patterns/CEmployee_pattern.py
@@ -21,9 +21,15 @@ class CEmployee_pattern:
         self.hourly_rate = int(hourly_rate)
         self.children = []
         self.activation_status = 0 #starts off as -1 when calculated later
+        self.i_self = -1
 
     def add_child(self, child):
         self.children.append(child)
+
+    def calc_i_self(self, i_self_p):
+        self.i_self = i_self_p
+        for i_child in range(len(self.children)):
+            self.children[i_child].calc_i_self(i_child)
 
     def find_by_name_recursive(self, name):
         if self.name == name:
@@ -69,3 +75,6 @@ class CEmployee_pattern:
             if first_activated_employee:
                 return first_activated_employee
         return None
+        
+    def delete_chosen_employee(self, index_employee_p):
+        
