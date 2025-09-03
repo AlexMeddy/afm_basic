26ae4ad5e1773b44f8f67fd3adc221aae58f1384
Alex Medeiros <alex.follador.medeiros@gmail.com>
2025-09-03 21:35:41 +1000

    draw for self

--- src/ai/CPeople/CPersonView.py
+++ src/ai/CPeople/CPersonView.py
@@ -42,10 +42,13 @@ class CPersonView:
                 return found
         return None
 
-    def draw_tree(self, surface, pygame_p):
-        # Draw rectangle for this node
+    def draw(self, surface, pygame_p):
         rect = pygame_p.Rect(self.x, self.y, self.w, self.h)
         pygame_p.draw.rect(surface, (255, 0, 0), rect, 2)
+        
+    def draw_tree(self, surface, pygame_p):
+        # Draw rectangle for this node
+        self.draw(surface, pygame_p)
         for child in self.children_list:
             child.draw_tree(surface, pygame_p)
 
